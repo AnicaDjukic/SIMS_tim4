845ef5548a5e45ee97d0db28c4f9cb50780e5411
AnicaDjukic <djukic.ra20.2018@uns.ac.rs>
2021-03-28 19:29:36 +0200

    Spojena grana serijalizacija pacijent

--- Projekat/bolnica/bolnica/obj/Debug/netcoreapp3.1/Forms/FormDodajPacijenta.g.i.cs
+++ Projekat/bolnica/bolnica/obj/Debug/netcoreapp3.1/Forms/FormDodajPacijenta.g.i.cs
@@ -223,7 +223,7 @@ namespace Bolnica.Forms {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -239,7 +239,7 @@ namespace Bolnica.Forms {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.3.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
