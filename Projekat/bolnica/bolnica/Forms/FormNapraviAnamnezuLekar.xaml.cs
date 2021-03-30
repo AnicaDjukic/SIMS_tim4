fa6619f35e96360ec11784f4229185a16d0aefbd
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2021-03-30 18:27:43 +0200

    mala promena

---
+++ Projekat/bolnica/bolnica/Forms/FormNapraviAnamnezuLekar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Bolnica.Forms
+{
+    /// <summary>
+    /// Interaction logic for FormNapraviAnamnezuLekar.xaml
+    /// </summary>
+    public partial class FormNapraviAnamnezuLekar : Window
+    {
+        public FormNapraviAnamnezuLekar()
+        {
+            InitializeComponent();
+        }
+    }
+}
