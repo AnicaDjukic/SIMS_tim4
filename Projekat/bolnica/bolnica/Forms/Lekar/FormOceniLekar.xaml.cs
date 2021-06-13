03e1bef924c1f7fa61c109ebe5535236b5f649bc
MilanGovedaricaRA15 <73367567+MilanGovedaricaRA15@users.noreply.github.com>
2021-06-13 13:08:24 +0200

    nesto

--- Projekat/bolnica/bolnica/Forms/Lekar/FormOceniLekar.xaml.cs
+++ Projekat/bolnica/bolnica/Forms/Lekar/FormOceniLekar.xaml.cs
@@ -59,5 +59,80 @@ namespace Bolnica.Forms
         {
             Close();
         }
+
+        private void RBVrloLosePritisnuto(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (rbVrloLose.IsChecked.Equals(true))
+                {
+                    rbVrloLose.IsChecked = false;
+                }
+                else
+                {
+                    rbVrloLose.IsChecked = true;
+                }
+            }
+        }
+
+        private void RBLosePritisnuto(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (rbLose.IsChecked.Equals(true))
+                {
+                    rbLose.IsChecked = false;
+                }
+                else
+                {
+                    rbLose.IsChecked = true;
+                }
+            }
+        }
+
+        private void RBVrloDobroPritisnuto(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (rbVrloDobro.IsChecked.Equals(true))
+                {
+                    rbVrloDobro.IsChecked = false;
+                }
+                else
+                {
+                    rbVrloDobro.IsChecked = true;
+                }
+            }
+        }
+
+        private void RBDobroPritisnuto(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (rbDobro.IsChecked.Equals(true))
+                {
+                    rbDobro.IsChecked = false;
+                }
+                else
+                {
+                    rbDobro.IsChecked = true;
+                }
+            }
+        }
+
+        private void RBOdlicnoPritisnuto(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (rbOdlicno.IsChecked.Equals(true))
+                {
+                    rbOdlicno.IsChecked = false;
+                }
+                else
+                {
+                    rbOdlicno.IsChecked = true;
+                }
+            }
+        }
     }
 }
